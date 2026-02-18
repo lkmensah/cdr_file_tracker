af1e5bff96ef957efbd395d58442a814cd650e82
Seek Men <seekmenfx@gmail.com>
2026-02-18 22:25:49 +0000

    Try fixing this error: `Runtime ReferenceError: Label is not defined. Er

--- src/components/new-file-dialog.tsx
+++ src/components/new-file-dialog.tsx
@@ -25,6 +25,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { createFile, updateFile } from '@/app/actions';
 import { useToast } from '@/hooks/use-toast';
 import { Combobox } from './ui/combobox';
